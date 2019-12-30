e124c5f4da1406555abdaf4a173d87c0b3af4a75
Laura Levy <llevy510@gmail.com>
2019-12-30 08:37:11 -0500

    created an adding function, results print on the 3rd line of output

--- main.go
+++ main.go
@@ -18,8 +18,13 @@ func main() {
 
     fmt.Println(i,"I reckon you're right on this one, Laura!",myboolean)
     ohYes()
+    fmt.Println(add(i,i))
 }
 
 func ohYes(){
   fmt.Println("I know that's right!")
+}
+
+func add(i, n int)int {
+  return i + n
 }
